3b43d0e55d7d396ae35aeeb0204a20652a720a90
Bruno Reis <bruno.ms.p@outlook.com>
2025-11-05 18:00:34 -0300

    email service finalizada

---
+++ src/main/java/com/Zenvy/Configuration/EmailService.java
@@ -0,0 +1,42 @@
+package com.Zenvy.Configuration;
+
+import com.Zenvy.exceptions.EmailException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public void enviarConfirmacaoDeReserva(String to, String subject, String body) {
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            message.setFrom("noreply@arbnb.com");
+
+            mailSender.send(message);
+        } catch (Exception ex) {
+            throw new EmailException("Erro ao enviar e-mail de confirmação", ex);
+        }
+    }
+
+    public void enviarCancelamentoDeReserva(String to, String subject, String body) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            message.setFrom("noreply@arbnb.com");
+
+            mailSender.send(message);
+        } catch (Exception ex) {
+            throw new EmailException("Erro ao enviar e-mail de cancelamento", ex);
+        }
+    }
+}
