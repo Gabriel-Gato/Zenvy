b2453497b311c1b14d0150c83624e732a2a6cbb3
Bruno Reis <bruno.ms.p@outlook.com>
2025-11-06 17:53:56 -0300

    controller de imovel pronta

---
+++ src/main/java/com/Zenvy/controllers/ImovelController.java
@@ -0,0 +1,61 @@
+package com.Zenvy.controllers;
+
+import com.Zenvy.models.Imovel;
+import com.Zenvy.services.ImovelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/imoveis")
+@RequiredArgsConstructor
+public class ImovelController {
+
+    private final ImovelService imovelService;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<Imovel> cadastrar(@RequestBody Imovel imovel) {
+        var novoImovel = imovelService.cadastrar(imovel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoImovel);
+    }
+
+    @PostMapping("/uploadImagem/{id}")
+    public ResponseEntity<Imovel> uploadImagem(
+            @PathVariable Long id,
+            @RequestParam("file") MultipartFile file) throws IOException {
+
+        var imovelAtualizado = imovelService.salvarImagem(id, file);
+        return ResponseEntity.ok(imovelAtualizado);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Imovel> buscarPorId(@PathVariable Long id) {
+        var imovel = imovelService.buscarPorId(id);
+        return ResponseEntity.ok(imovel);
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Imovel>> listarTodos() {
+        return ResponseEntity.ok(imovelService.listarTodos());
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<Imovel> atualizar(
+            @PathVariable Long id,
+            @RequestBody Imovel imovelAtualizado) {
+
+        var imovel = imovelService.atualizar(id, imovelAtualizado);
+        return ResponseEntity.ok(imovel);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> deletarPorId(@PathVariable Long id) {
+        imovelService.deletarPorId(id);
+        return ResponseEntity.noContent().build();
+    }
+}
