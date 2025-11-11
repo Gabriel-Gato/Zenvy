8f2627abe12068b3bda7acd0775ed6bbc8009694
Bruno Reis <bruno.ms.p@outlook.com>
2025-11-11 19:45:09 -0300

    transformei esse dto num record e adicionei as validações

--- src/main/java/com/Zenvy/dto/LoginRequest.java
+++ src/main/java/com/Zenvy/dto/LoginRequest.java
@@ -1,11 +1,20 @@
 package com.Zenvy.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
-@Getter
-@Setter
-public class LoginRequest {
-    private String email;
-    private String senha;
+public record LoginRequest(
+
+        @NotBlank(message = "O email é obrigatório")
+        @Email(message = "Email inválido")
+        String email,
+
+        @NotBlank(message = "A senha é obrigatória")
+        @Size(min = 8, max = 100, message = "A senha deve ter entre 8 e 100 caracteres")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]+$",
+                message = "A senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais")
+        String senha
+) {
 }
